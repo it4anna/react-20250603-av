25699c6ab2e84af74b0755948c5795b0121330c4
Hanna Vostrykova <it4anna@gmail.com>
2025-11-05 10:53:04 -0500

    Home WOrk 3: Fix quantity buttons

--- src/hooks/useQuantity.ts
+++ src/hooks/useQuantity.ts
@@ -4,7 +4,7 @@ import {MAX_QUANITITY, MIN_QUANITITY} from '../utils/constants'
 export const useQuantity = (initialState: number) => {
   const [quantity, setQuantity] = useState(initialState);
   const increaseQuantity = () => quantity < MAX_QUANITITY && setQuantity(quantity + 1)
-  const decreaseQuantity = () => quantity > MIN_QUANITITY && setQuantity(quantity - 2)
+  const decreaseQuantity = () => quantity > MIN_QUANITITY && setQuantity(quantity - 1)
 
   return {quantity, increaseQuantity, decreaseQuantity}
 }
